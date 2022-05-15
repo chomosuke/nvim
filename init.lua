97d62f087f05adf9b2a4a082931b216da57bb747
chomosuke <a13323600@gmail.com>
2022-05-15 22:09:17 +1000

    lsp half configured

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- helpers for keymap
-function map(mode, keys, mapping)
-	vim.api.nvim_set_keymap(mode, keys, mapping, { noremap = true })
+local function map(mode, keys, mapping)
+  vim.api.nvim_set_keymap(mode, keys, mapping, { noremap = true })
 end
 
-function imap(keys, mapping)
-	map('i', keys, mapping)
+local function imap(keys, mapping)
+  map('i', keys, mapping)
 end
 
-function nmap(keys, mapping)
-	map('n', keys, mapping)
+local function nmap(keys, mapping)
+  map('n', keys, mapping)
 end
 
 -- map shift-enter & shift-space to <Esc>
@@ -24,3 +24,31 @@ vim.opt.number = true
 
 -- for packer
 require('plugins')
+
+-- for lsp
+local lsp_status = require 'lsp-status'
+local lspconfig = require 'lspconfig'
+
+lsp_status.register_progress()
+
+vim.cmd [[
+  autocmd BufEnter * lua require 'completion'.on_attach()
+]]
+
+local function on_attach(client)
+  lsp_status.on_attach(client)
+end
+
+local capabilities = lsp_status.capabilities
+
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lsp_status.status()
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,22 +1,28 @@
-vim.cmd([[
-	augroup packer_user_config
-	  autocmd!
-	  autocmd BufWritePost plugins.lua source <afile> | PackerCompile
-	augroup end
-]])
+-- for auto PackerSync
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
 
-return require('packer').startup(function(use)
-	-- for packer to host itself
-	use 'wbthomason/packer.nvim'
+return require 'packer'.startup(function(use)
+  -- for packer to host itself
+  use 'wbthomason/packer.nvim'
 
-	-- for git
-	use {
-		'lewis6991/gitsigns.nvim',
-		config = function()
-			require('gitsigns').setup {
-				signcolumn = false,
-				numhl = true,
-			}
-		end
-	}
+  -- for git
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require 'gitsigns'.setup {
+        signcolumn = false,
+        numhl = true,
+      }
+    end
+  }
+
+  -- for language servers
+  use 'neovim/nvim-lspconfig'
+  use 'nvim-lua/lsp-status.nvim'
+  use 'nvim-lua/completion-nvim'
 end)

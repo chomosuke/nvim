d69270e0a52e750b61f1e04dcc8734057738973c
Richard Li <richard.li@detectorinspector.com.au>
2022-06-01 23:57:09 +1000

    map shift-enter to <Esc> in all modes & mapped <Leader>cb current buffer fuzzy find.

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,24 +1,32 @@
 -- helpers for keymap
-local function map(mode, keys, mapping)
-vim.api.nvim_set_keymap(mode, keys, mapping, { noremap = true })
+local function keymap(mode, keys, mapping)
+  vim.api.nvim_set_keymap(mode, keys, mapping, { noremap = true })
+end
+
+-- for all modes
+local function amap(keys, mapping)
+  keymap('', keys, mapping)
+  keymap('!', keys, mapping)
+  keymap('t', keys, mapping)
 end
 
 local function imap(keys, mapping)
-map('i', keys, mapping)
+  keymap('i', keys, mapping)
 end
 
 local function nmap(keys, mapping)
-map('n', keys, mapping)
+  keymap('n', keys, mapping)
 end
 
--- map shift-enter & shift-space to <Esc>
-imap('<S-CR>', '<Esc>')
+-- map shift-enter to <Esc>
+amap('<S-CR>', '<Esc>')
 
 -- map space to leader
 vim.g.mapleader = ' '
 
 -- map <Leader>fd to open telescope fd
 nmap('<Leader>fd', ':Telescope fd<CR>')
+nmap('<Leader>cb', ':Telescope current_buffer_fuzzy_find<CR>')
 
 -- mapping for NvimTree
 nmap('<Leader>n', ':NvimTreeToggle<CR>')

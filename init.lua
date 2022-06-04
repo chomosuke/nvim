52b2e713dc6d3a5cb6000e6a0bb0da40d4b154b0
chomosuke <a13323600@gmail.com>
2022-06-05 00:43:30 +1200

    not using vscode-neovim anymore so no more check if in vscode

--- init.lua
+++ init.lua
@@ -1,24 +1,23 @@
-if not vim.g.vscode then
-  require 'plugins'
-  require 'config'
-  require 'mapping'
+require 'plugins'
+require 'config'
+require 'mapping'
 
-  -- for machine specific configuration
-  local function isModuleAvailable(name)
-    if package.loaded[name] then
-      return true
-    else
-      for _, searcher in ipairs(package.searchers or package.loaders) do
-        local loader = searcher(name)
-        if type(loader) == 'function' then
-          package.preload[name] = loader
-          return true
-        end
+-- for machine specific configuration
+local function isModuleAvailable(name)
+  if package.loaded[name] then
+    return true
+  else
+    for _, searcher in ipairs(package.searchers or package.loaders) do
+      local loader = searcher(name)
+      if type(loader) == 'function' then
+        package.preload[name] = loader
+        return true
       end
-      return false
     end
+    return false
   end
-  if isModuleAvailable 'machine_specific' then
-    require 'machine_specific'
-  end
+end
+
+if isModuleAvailable 'machine_specific' then
+  require 'machine_specific'
 end

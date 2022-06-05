069787f820daa53a4b4e96473c72494643e39870
chomosuke <a13323600@gmail.com>
2022-06-05 12:25:32 +1200

    fixed visual mode doesn't execute command (specifically made hop work in visual mode)

--- lua/plugins_config/gitsigns.lua
+++ lua/plugins_config/gitsigns.lua
@@ -25,20 +25,20 @@ return function()
       end, { expr = true })
 
       -- Actions
-      map({ 'n', 'v' }, '<Leader>gs', ':Gitsigns stage_hunk<CR>')
-      map({ 'n', 'v' }, '<Leader>gr', ':Gitsigns reset_hunk<CR>')
-      map('n', '<Leader>gS', ':Gitsigns stage_buffer<CR>')
-      map('n', '<Leader>gu', ':Gitsigns undo_stage_hunk<CR>')
-      map('n', '<Leader>gR', ':Gitsigns reset_buffer<CR>')
-      map('n', '<Leader>gp', ':Gitsigns preview_hunk<CR>')
+      map({ 'n', 'v' }, '<Leader>gs', '<cmd>Gitsigns stage_hunk<CR>')
+      map({ 'n', 'v' }, '<Leader>gr', '<cmd>Gitsigns reset_hunk<CR>')
+      map('n', '<Leader>gS', '<cmd>Gitsigns stage_buffer<CR>')
+      map('n', '<Leader>gu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+      map('n', '<Leader>gR', '<cmd>Gitsigns reset_buffer<CR>')
+      map('n', '<Leader>gp', '<cmd>Gitsigns preview_hunk<CR>')
       map('n', '<Leader>gb', function() gs.blame_line { full = true } end)
-      map('n', '<Leader>gtb', ':Gitsigns toggle_current_line_blame<CR>')
-      map('n', '<Leader>gd', ':Gitsigns diffthis<CR>')
+      map('n', '<Leader>gtb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+      map('n', '<Leader>gd', '<cmd>Gitsigns diffthis<CR>')
       map('n', '<Leader>gD', function() gs.diffthis('~') end)
-      map('n', '<Leader>gtd', ':Gitsigns toggle_deleted<CR>')
+      map('n', '<Leader>gtd', '<cmd>Gitsigns toggle_deleted<CR>')
 
       -- Text object
-      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      map({ 'o', 'x' }, 'ih', '<cmd><C-U>Gitsigns select_hunk<CR>')
     end
   }
 end

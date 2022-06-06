e0af0d429a15f66892cae02df97154c0e95846a0
Richard Li <richard.li@detectorinspector.com.au>
2022-06-06 10:56:12 +1000

    global statusline

--- lua/plugins_config/lualine.lua
+++ lua/plugins_config/lualine.lua
@@ -1,22 +1,21 @@
 return function()
+  local sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'filetype' },
+    lualine_c = { 'lsp_progress', 'diagnostics' },
+    lualine_x = { 'diff' },
+    lualine_y = { 'branch' },
+    lualine_z = { 'progress' },
+  }
   require 'lualine'.setup {
     options = {
       component_separators = '',
       section_separators = '',
-      disabled_filetypes = { 'NvimTree', 'packer', 'dashboard' },
+      disabled_filetypes = { 'dashboard' },
       always_divide_middle = false,
+      globalstatus = true,
     },
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'filetype' },
-      lualine_c = { 'lsp_progress', 'diagnostics' },
-      lualine_x = { 'diff' },
-      lualine_y = { 'branch' },
-      lualine_z = { 'progress' },
-    },
-    inactive_sections = {
-      lualine_c = { 'filetype' },
-      lualine_x = {},
-    }
+    sections = sections,
+    inactive_sections = sections,
   }
 end

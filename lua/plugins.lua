49be1fb31668f744667b91c0d1b1e6aeff0add8f
Richard Li <richard.li@detectorinspector.com.au>
2022-06-06 13:28:25 +1000

    packer open window

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,175 +20,187 @@ vim.cmd [[
   augroup end
 ]]
 
-return require 'packer'.startup(function(use)
-  -- for packer to host itself
-  use 'wbthomason/packer.nvim'
+return require 'packer'.startup({
+  function(use)
+    -- for packer to host itself
+    use 'wbthomason/packer.nvim'
 
-  -- prereq for many plugins
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+    -- prereq for many plugins
+    use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
+    }
 
-  -- for git
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = require 'plugins_config.gitsigns',
-  }
-  use 'tpope/vim-fugitive'
+    -- for git
+    use {
+      'lewis6991/gitsigns.nvim',
+      config = require 'plugins_config.gitsigns',
+    }
+    use 'tpope/vim-fugitive'
 
-  -- for language servers
-  use 'williamboman/nvim-lsp-installer'
-  use {
-    'neovim/nvim-lspconfig',
-    config = require 'plugins_config.nvim-lspconfig',
-  }
-  use {
-    'simrat39/rust-tools.nvim',
-    config = require 'rust-tools'.setup {}
-  }
-  use {
-    'ms-jpq/coq_nvim',
-    branch = 'coq',
-    requires = {
-      { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
-      { 'ms-jpq/coq.thirdparty', branch = '3p' },
-    },
-    config = function()
-      vim.g.coq_settings = {
-        auto_start = true,
-      }
-      require 'coq'
-    end,
-  }
+    -- for language servers
+    use 'williamboman/nvim-lsp-installer'
+    use {
+      'neovim/nvim-lspconfig',
+      config = require 'plugins_config.nvim-lspconfig',
+    }
+    use {
+      'simrat39/rust-tools.nvim',
+      config = require 'rust-tools'.setup {}
+    }
+    use {
+      'ms-jpq/coq_nvim',
+      branch = 'coq',
+      requires = {
+        { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
+        { 'ms-jpq/coq.thirdparty', branch = '3p' },
+      },
+      config = function()
+        vim.g.coq_settings = {
+          auto_start = true,
+          display = {
+            icons = {
+              mode = 'short',
+            },
+          },
+        }
+        require 'coq'
+      end,
+    }
 
-  -- for autopairs
-  use {
-    'windwp/nvim-autopairs',
-    config = require 'plugins_config/nvim-autopairs',
-  }
+    -- for autopairs
+    use {
+      'windwp/nvim-autopairs',
+      config = require 'plugins_config/nvim-autopairs',
+    }
 
-  -- for indentation
-  use {
-    'nmac427/guess-indent.nvim',
-    config = function()
-      require 'guess-indent'.setup {}
-    end,
-  }
+    -- for indentation
+    use {
+      'nmac427/guess-indent.nvim',
+      config = function()
+        require 'guess-indent'.setup {}
+      end,
+    }
 
-  -- for telescope finding files
-  use {
-    'nvim-telescope/telescope.nvim',
-    config = require 'plugins_config.telescope',
-    requires = 'nvim-lua/plenary.nvim',
-  }
+    -- for telescope finding files
+    use {
+      'nvim-telescope/telescope.nvim',
+      config = require 'plugins_config.telescope',
+      requires = 'nvim-lua/plenary.nvim',
+    }
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {
-      'kyazdani42/nvim-web-devicons',
-      'arkav/lualine-lsp-progress',
-    },
-    config = require 'plugins_config/lualine',
-  }
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = {
+        'kyazdani42/nvim-web-devicons',
+        'arkav/lualine-lsp-progress',
+      },
+      config = require 'plugins_config/lualine',
+    }
 
-  -- for indentation guides
-  use {
-    'lukas-reineke/indent-blankline.nvim',
-    config = function()
-      require 'indent_blankline'.setup {}
-    end,
-  }
+    -- for indentation guides
+    use {
+      'lukas-reineke/indent-blankline.nvim',
+      config = function()
+        require 'indent_blankline'.setup {}
+      end,
+    }
 
-  -- for theme
-  use {
-    'Mofiqul/vscode.nvim',
-    config = function()
-      vim.g.vscode_italic_comment = 1
-      vim.g.vscode_disable_nvimtree_bg = true
-    end,
-  }
-  use {
-    'navarasu/onedark.nvim',
-    config = function()
-      require 'onedark'.load()
-    end,
-  }
+    -- for theme
+    use {
+      'Mofiqul/vscode.nvim',
+      config = function()
+        vim.g.vscode_italic_comment = 1
+        vim.g.vscode_disable_nvimtree_bg = true
+      end,
+    }
+    use {
+      'navarasu/onedark.nvim',
+      config = function()
+        require 'onedark'.load()
+      end,
+    }
 
-  -- for file explorer
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons', -- for file icon
-    config = require 'plugins_config.nvim-tree',
-  }
+    -- for file explorer
+    use {
+      'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons', -- for file icon
+      config = require 'plugins_config.nvim-tree',
+    }
 
-  -- for smooth scrolling
-  use {
-    'karb94/neoscroll.nvim',
-    config = function()
-      require 'neoscroll'.setup()
-    end,
-  }
+    -- for smooth scrolling
+    use {
+      'karb94/neoscroll.nvim',
+      config = function()
+        require 'neoscroll'.setup()
+      end,
+    }
 
-  -- for buffer as tabs
-  use {
-    'akinsho/bufferline.nvim',
-    tag = 'v2.*',
-    requires = 'kyazdani42/nvim-web-devicons',
-    config = function()
-      require 'bufferline'.setup {
-        options = {
-          show_buffer_close_icons = false,
-          show_close_icon = false,
-        },
-      }
-    end,
-  }
+    -- for buffer as tabs
+    use {
+      'akinsho/bufferline.nvim',
+      tag = 'v2.*',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function()
+        require 'bufferline'.setup {
+          options = {
+            show_buffer_close_icons = false,
+            show_close_icon = false,
+          },
+        }
+      end,
+    }
 
-  -- for keybinding hint
-  use {
-    'folke/which-key.nvim',
-    config = function()
-      require('which-key').setup {}
+    -- for keybinding hint
+    use {
+      'folke/which-key.nvim',
+      config = function()
+        require('which-key').setup {}
+      end
+    }
+
+    -- better escape mapping
+    use {
+      'max397574/better-escape.nvim',
+      config = function()
+        require 'better_escape'.setup {
+          mapping = { ',.' },
+          timeout = 128,
+        }
+      end,
+    }
+
+    -- hop navigation
+    use {
+      'phaazon/hop.nvim',
+      config = function()
+        require 'hop'.setup { keys = 'aoeuhtnsidpgcrlyfqjkmwvzxb' }
+      end
+    }
+
+    -- dashboard
+    use {
+      'glepnir/dashboard-nvim',
+      config = require 'plugins_config.dashboard',
+    }
+
+    -- for comment toggle
+    use {
+      'numToStr/Comment.nvim',
+      config = function()
+        require('Comment').setup()
+      end
+    }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      require 'packer'.sync()
     end
-  }
-
-  -- better escape mapping
-  use {
-    'max397574/better-escape.nvim',
-    config = function()
-      require 'better_escape'.setup {
-        mapping = { ',.' },
-        timeout = 128,
-      }
-    end,
-  }
-
-  -- hop navigation
-  use {
-    'phaazon/hop.nvim',
-    config = function()
-      require 'hop'.setup { keys = 'aoeuhtnsidpgcrlyfqjkmwvzxb' }
-    end
-  }
-
-  -- dashboard
-  use {
-    'glepnir/dashboard-nvim',
-    config = require 'plugins_config.dashboard',
-  }
-
-  -- for comment toggle
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require 'packer'.sync()
-  end
-end)
+  end,
+  config = {
+    display = {
+      open_fn = require('packer.util').float,
+    }
+  },
+})

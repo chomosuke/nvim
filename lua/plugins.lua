7c184e58f65bddd2ef481e628586b0bc273a74c6
Richard Li <richard.li@detectorinspector.com.au>
2022-06-13 16:44:27 +1000

    markdown preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -243,6 +243,12 @@ return require 'packer'.startup({
       end,
     }
 
+    -- markdown preview
+    use({
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+    })
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then

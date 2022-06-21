8c505c6c51224f08ff16a0a48bf6b4660d223e9f
chomosuke <a13323600@gmail.com>
2022-06-21 22:12:17 +1200

    added back smooth scroll after own version fixed the issue

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -153,13 +153,13 @@ return require 'packer'.startup({
       config = require 'plugins_config.nvim-tree',
     }
 
-    -- -- for smooth scrolling
-    -- use {
-    --   'karb94/neoscroll.nvim',
-    --   config = function()
-    --     require 'neoscroll'.setup()
-    --   end,
-    -- }
+    -- for smooth scrolling
+    use {
+      'chomosuke/neoscroll.nvim',
+      config = function()
+        require 'neoscroll'.setup()
+      end,
+    }
 
     -- for buffer as tabs
     use {

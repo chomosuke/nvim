6684702043250dbea5e31c82cd82f730a155fa0b
chomosuke <a13323600@gmail.com>
2022-06-23 02:58:47 +1200

    using treesitter scope for indent

--- lua/plugins_config/indent-blankline.lua
+++ lua/plugins_config/indent-blankline.lua
@@ -2,6 +2,7 @@ return function()
   require 'indent_blankline'.setup {
     use_treesitter = true,
     show_current_context = true,
+    use_treesitter_scope = true,
   }
 
   require 'indent_blankline.utils'.reset_highlights()

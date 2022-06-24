a7e7323533dcf7cadde21876b4658ce14366d2b2
chomosuke <a13323600@gmail.com>
2022-06-24 16:44:21 +1200

    showing fileformat in statusline

--- lua/plugins_config/lualine.lua
+++ lua/plugins_config/lualine.lua
@@ -3,8 +3,24 @@ return function()
     lualine_a = { 'mode' },
     lualine_b = { 'filetype' },
     lualine_c = { 'lsp_progress', 'diagnostics' },
-    lualine_x = { 'diff' },
-    lualine_y = { 'branch' },
+    lualine_x = {
+      'diff',
+      'branch',
+    },
+    lualine_y = {
+      {
+        'fileformat',
+        icons_enabled = false,
+        fmt = function(ff)
+          if ff == 'unix' then
+            ff = 'LF'
+          elseif ff == 'dos' then
+            ff = 'CRLF'
+          end
+          return ff
+        end,
+      },
+    },
     lualine_z = { 'progress' },
   }
   require 'lualine'.setup {

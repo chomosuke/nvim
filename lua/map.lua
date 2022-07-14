5963b4974afdeafc736a653534fec6a4a05288a6
chomosuke <a13323600@gmail.com>
2022-07-15 02:26:16 +1200

    all mapping taking in opts

--- lua/map.lua
+++ lua/map.lua
@@ -1,34 +1,40 @@
 local M = {}
 
-local function keymap(mode, keys, mapping)
-  vim.keymap.set(mode, keys, mapping, { noremap = true })
+local function keymap(mode, keys, mapping, options)
+  local opts = { noremap = true }
+  if options ~= nil then
+    for key, value in pairs(options) do
+      opts[key] = value
+    end
+  end
+  vim.keymap.set(mode, keys, mapping, opts)
 end
 
 -- for all modes
-function M.amap(keys, mapping)
-  keymap('', keys, mapping)
-  keymap('!', keys, mapping)
-  keymap('t', keys, mapping)
+function M.amap(keys, mapping, opts)
+  keymap('', keys, mapping, opts)
+  keymap('!', keys, mapping, opts)
+  keymap('t', keys, mapping, opts)
 end
 
-function M.cmap(keys, mapping)
-  keymap('c', keys, mapping)
+function M.cmap(keys, mapping, opts)
+  keymap('c', keys, mapping, opts)
 end
 
-function M.tmap(keys, mapping)
-  keymap('t', keys, mapping)
+function M.tmap(keys, mapping, opts)
+  keymap('t', keys, mapping, opts)
 end
 
-function M.imap(keys, mapping)
-  keymap('i', keys, mapping)
+function M.imap(keys, mapping, opts)
+  keymap('i', keys, mapping, opts)
 end
 
-function M.nmap(keys, mapping)
-  keymap('n', keys, mapping)
+function M.nmap(keys, mapping, opts)
+  keymap('n', keys, mapping, opts)
 end
 
-function M.map(keys, mapping)
-  keymap('', keys, mapping)
+function M.map(keys, mapping, opts)
+  keymap('', keys, mapping, opts)
 end
 
 return M

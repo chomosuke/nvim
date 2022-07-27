8e0ef360ee7204c2b42363db047a18c9ff1545d9
chomosuke <a13323600@gmail.com>
2022-07-27 11:18:27 +1000

    file-tree not highlighting opened, adaptive size, preserve_window_proportions & not showing folder_arrow

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -1,34 +1,43 @@
 return function(use)
-use {
-  'kyazdani42/nvim-tree.lua',
-  requires = 'kyazdani42/nvim-web-devicons', -- for file icon
-  config = function()
-    require 'nvim-tree'.setup {
-      git = {
-        ignore = false,
-      },
-      renderer = {
-        highlight_git = true,
-        highlight_opened_files = 'icon',
-        indent_markers = {
-          enable = true,
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons', -- for file icon
+    config = function()
+      require 'nvim-tree'.setup {
+        view = {
+          adaptive_size = true,
+          preserve_window_proportions = true,
         },
-      },
-      diagnostics = {
-        enable = true,
-        icons = {
-          hint = '',
-          info = '',
-          warning = '',
-          error = '',
-        }, show_on_dirs = true,
-      },
-    }
+        renderer = {
+          highlight_git = true,
+          indent_markers = {
+            enable = true,
+          },
+          icons = {
+            show = {
+              folder_arrow = false,
+            },
+          },
+        },
+        diagnostics = {
+          enable = true,
+          icons = {
+            hint = '',
+            info = '',
+            warning = '',
+            error = '',
+          },
+          show_on_dirs = true,
+        },
+        git = {
+          ignore = false,
+        },
+      }
 
-    -- mapings
-    local map = require 'map'
-    map.nmap('<Leader>n', '<cmd>NvimTreeToggle<CR>')
-    map.nmap('<Leader>r', '<cmd>NvimTreeRefresh<CR>')
-  end,
-}
+      -- mapings
+      local map = require 'map'
+      map.nmap('<Leader>n', '<cmd>NvimTreeToggle<CR>')
+      map.nmap('<Leader>r', '<cmd>NvimTreeRefresh<CR>')
+    end,
+  }
 end

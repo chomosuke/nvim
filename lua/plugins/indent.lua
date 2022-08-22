ad86801c6b0491be0cdcbcbca1b54112c0141c58
chomosuke <a13323600@gmail.com>
2022-08-22 15:08:02 +1000

    refactored indent.lua

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -2,6 +2,7 @@ return function(use)
   use {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
+      --   helper functions definition   --
       local function fromhex(str)
         local chars = str:gsub('..', function(cc)
           return string.char(tonumber(cc, 16))
@@ -26,43 +27,58 @@ return function(use)
         end))
       end
 
-      local highlight_group = vim.fn.synIDtrans(vim.fn.hlID 'Normal')
-      local highlight = {
-        vim.fn.synIDattr(highlight_group, 'bg', 'gui'),
-        vim.fn.synIDattr(highlight_group, 'bg', 'cterm'),
-      }
+      local function set_highlight(highlight_name1, highlight_name2, diff_func)
+        local highlight_group = vim.fn.synIDtrans(vim.fn.hlID 'Normal')
+        local highlight = {
+          vim.fn.synIDattr(highlight_group, 'bg', 'gui'),
+          vim.fn.synIDattr(highlight_group, 'bg', 'cterm'),
+        }
 
-      for i = 1, #highlight do
-        if highlight[i] == '' then
-          highlight[i] = 'NONE'
-        end
-      end
-
-      vim.cmd(
-        string.format(
-          'highlight IndentBlanklineIndent1 guibg=%s ctermbg=%s gui=nocombine cterm=nocombine',
-          highlight[1],
-          highlight[2]
-        )
-      )
-
-      for i = 1, #highlight do
-        if highlight[i] ~= 'NONE' then
-          highlight[i] = string.sub(highlight[i], 2, 7)
-          highlight[i] = fromhex(highlight[i])
-          for j = 1, #highlight[i] do
-            highlight[i][j] = math.floor(highlight[i][j] * 0.88)
+        for i = 1, #highlight do
+          if highlight[i] == '' then
+            highlight[i] = 'NONE'
           end
-          highlight[i] = '#' .. tohex(highlight[i])
         end
-      end
 
-      vim.cmd(
-        string.format(
-          'highlight IndentBlanklineIndent2 guibg=%s ctermbg=%s gui=nocombine cterm=nocombine',
-          highlight[1],
-          highlight[2]
+        vim.cmd(
+          string.format(
+            'highlight %s guibg=%s ctermbg=%s gui=nocombine cterm=nocombine',
+            highlight_name1,
+            highlight[1],
+            highlight[2]
+          )
         )
+
+        for i = 1, #highlight do
+          if highlight[i] ~= 'NONE' then
+            highlight[i] = string.sub(highlight[i], 2, 7)
+            highlight[i] = fromhex(highlight[i])
+            for j = 1, #highlight[i] do
+              highlight[i][j] = diff_func(highlight[i][j])
+            end
+            highlight[i] = '#' .. tohex(highlight[i])
+          end
+        end
+
+        vim.cmd(
+          string.format(
+            'highlight %s guibg=%s ctermbg=%s gui=nocombine cterm=nocombine',
+            highlight_name2,
+            highlight[1],
+            highlight[2]
+          )
+        )
+      end
+      -- helper functions definition end --
+
+      local name1 = 'IndentBlanklineIndent1'
+      local name2 = 'IndentBlanklineIndent2'
+      set_highlight(
+        name1,
+        name2,
+        function(hi)
+          return math.max(hi - 4, 0)
+        end
       )
 
       require 'indent_blankline'.setup {
@@ -70,12 +86,12 @@ return function(use)
         show_trailing_blankline_indent = false,
         char = '',
         char_highlight_list = {
-          'IndentBlanklineIndent1',
-          'IndentBlanklineIndent2',
+          name1,
+          name2,
         },
         space_char_highlight_list = {
-          'IndentBlanklineIndent1',
-          'IndentBlanklineIndent2',
+          name1,
+          name2,
         },
         filetype_exclude = {
           'lspinfo',

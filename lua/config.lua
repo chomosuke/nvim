93f25fe5a2e1564793b517ddc1b8e8bea192d8eb
chomosuke <a13323600@gmail.com>
2022-08-22 12:57:29 +1000

    changed listchars to include tab and nbsp

--- lua/config.lua
+++ lua/config.lua
@@ -3,7 +3,7 @@ vim.opt.mouse = nil
 
 -- render space & tabs
 vim.opt.list = true
-vim.opt.listchars = 'trail:·'
+vim.opt.listchars = 'tab: ,trail:·,nbsp:⍽'
 
 -- show relative line numbers
 vim.opt.relativenumber = true

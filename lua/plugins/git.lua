e0f85d434dcd46194862683118e4d6f724db08be
chomosuke <a13323600@gmail.com>
2022-08-31 20:53:37 +1000

    reorganized gitsigns mapping with which-key

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,10 +6,12 @@ return function(use)
         numhl = true,
         on_attach = function(bufnr)
           local gs = package.loaded.gitsigns
+          local wk = require 'which-key'
 
           local function map(mode, l, r, opts)
             opts = opts or {}
             opts.buffer = bufnr
+            opts.noremap = true
             vim.keymap.set(mode, l, r, opts)
           end
 
@@ -18,29 +20,53 @@ return function(use)
             if vim.wo.diff then return ']c' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
-          end, { expr = true })
+          end, {
+            expr = true,
+            desc = 'next hunk',
+          })
 
           map('n', '[c', function()
             if vim.wo.diff then return '[c' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
-          end, { expr = true })
+          end, {
+            expr = true,
+            desc = 'prev hunk',
+          })
 
           -- Actions
-          map({ 'n', 'v' }, '<leader>gs', ':Gitsigns stage_hunk<CR>')
-          map({ 'n', 'v' }, '<leader>gr', ':Gitsigns reset_hunk<CR>')
-          map('n', '<leader>gS', gs.stage_buffer)
-          map('n', '<leader>gu', gs.undo_stage_hunk)
-          map('n', '<leader>gR', gs.reset_buffer)
-          map('n', '<leader>gp', gs.preview_hunk)
-          map('n', '<leader>gb', function() gs.blame_line { full = true } end)
-          map('n', '<leader>gtb', gs.toggle_current_line_blame)
-          map('n', '<leader>gd', gs.diffthis)
-          map('n', '<leader>gD', function() gs.diffthis('~') end)
-          map('n', '<leader>gtd', gs.toggle_deleted)
+          local nv_actions_mappings = {
+            ['<leader>g'] = {
+              name = 'git',
+              s = { ':Gitsigns stage_hunk<CR>', 'stage hunk' },
+              r = { ':Gitsigns reset_hunk<CR>', 'reset hunk' },
+            },
+          }
+          wk.register(nv_actions_mappings, { buffer = bufnr })
+          wk.register(nv_actions_mappings, { mode = 'v', buffer = bufnr })
+          wk.register({
+            ['<leader>g'] = {
+              name = 'git',
+              S = { gs.stage_buffer, 'stage buffer' },
+              u = { gs.undo_stage_hunk, 'undo stage hunk' },
+              R = { gs.reset_buffer, 'reset buffer' },
+              p = { gs.preview_hunk, 'preview hunk' },
+              b = { function() gs.blame_line { full = true } end, 'blame line' },
+              tb = { gs.toggle_current_line_blame, 'toggle current line blame' },
+              d = { gs.diffthis, 'diff this' },
+              D = { function() gs.diffthis('~') end, 'diff this' },
+              td = { gs.toggle_deleted, 'toggle deleted' },
+            },
+          }, {
+            buffer = bufnr,
+          })
 
           -- Text object
-          map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+          local text_object_mappings = {
+            ih = { ':<C-U>Gitsigns select_hunk<CR>', 'select hunk' }
+          }
+          wk.register(text_object_mappings, { buffer = bufnr, mode = 'o' })
+          wk.register(text_object_mappings, { buffer = bufnr, mode = 'x' })
         end
       }
     end,

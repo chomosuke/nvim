f645298c564e4e5f488a308e22bfc6be2c0c2232
chomosuke <a13323600@gmail.com>
2022-09-10 20:41:54 +1000

    Removed auto line wrap in gitcommit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,9 @@
+-- get rid of auto linewrap in git commit
+require 'util'.create_autocmds(
+  'git_commit_no_tw',
+  { { 'FileType', { command = 'setlocal tw=0', pattern =  'gitcommit' } } }
+)
+
 return function(use)
   use {
     'lewis6991/gitsigns.nvim',

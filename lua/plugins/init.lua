ef30ad5f4938eab765a942b1fc0687fdadc4c74a
chomosuke <a13323600@gmail.com>
2022-10-12 17:21:24 +1100

    do not autoclose markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -132,6 +132,9 @@ return require 'packer'.startup {
     use {
       'iamcco/markdown-preview.nvim',
       run = function() vim.fn['mkdp#util#install']() end,
+      config = function()
+        vim.g.mkdp_auto_close = 0
+      end,
     }
 
     -- terminal

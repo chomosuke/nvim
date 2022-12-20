b3e147a848e0e8f2fe85f4dfdfdfcadcc54f2f3c
chomosuke <a13323600@gmail.com>
2022-12-20 20:16:48 +1300

    neodev

--- lua/plugins/lsp-dap.lua
+++ lua/plugins/lsp-dap.lua
@@ -174,6 +174,7 @@ return function(use)
       }
 
       -- setup lsps
+      require('neodev').setup {}
       local lspconfig = require 'lspconfig'
       require('mason-lspconfig').setup_handlers {
         function(server_name)
@@ -207,17 +208,6 @@ return function(use)
             tools = {
               hover_actions = {
                 auto_focus = true,
-              },
-            },
-          }
-        end,
-        sumneko_lua = function()
-          lspconfig.sumneko_lua.setup {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-              workspace = {
-                library = vim.api.nvim_get_runtime_file('', true),
               },
             },
           }
@@ -259,6 +249,7 @@ return function(use)
       'akinsho/flutter-tools.nvim',
       'nvim-lua/plenary.nvim',
       'chomosuke/ltex_extra.nvim',
+      'folke/neodev.nvim',
     },
   }
 

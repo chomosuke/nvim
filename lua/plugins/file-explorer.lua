3fdcdfe725610365ee0a570de20495990d5a2e8f
chomosuke <a13323600@gmail.com>
2023-02-03 21:04:21 +1100

    updated and addressed nvim-tree's changes

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -4,8 +4,29 @@ return {
     lazy = false,
     dependencies = { 'nvim-tree/nvim-web-devicons' }, -- for file icon
     config = function()
+      require('util').create_autocmds('nvim_tree_open_on_VimEnter', {
+        {
+          event = 'VimEnter',
+          opts = {
+            callback = function(data)
+              -- buffer is a directory
+              local directory = vim.fn.isdirectory(data.file) == 1
+              if not directory then
+                return
+              end
+              -- create a new, empty buffer
+              vim.cmd.enew()
+              -- wipe the directory buffer
+              vim.cmd.bw(data.buf)
+              -- change to the directory
+              vim.cmd.cd(data.file)
+              -- open the tree
+              require('nvim-tree.api').tree.open()
+            end,
+          },
+        },
+      })
       require('nvim-tree').setup {
-        open_on_setup = true,
         view = {
           mappings = {
             list = {

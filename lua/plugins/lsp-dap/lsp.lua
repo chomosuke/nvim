7436fd337774c7e969e17e787a99d0d1f7284a44
chomosuke <a13323600@gmail.com>
2023-02-15 22:00:10 +1100

    fix sumneko_lua rename

--- lua/plugins/lsp-dap/lsp.lua
+++ lua/plugins/lsp-dap/lsp.lua
@@ -35,8 +35,8 @@ require('mason-lspconfig').setup_handlers {
       capabilities = capabilities,
     }
   end,
-  sumneko_lua = function()
-    lspconfig.sumneko_lua.setup {
+  lua_ls = function()
+    lspconfig.lua_ls.setup {
       on_attach = on_attach,
       capabilities = capabilities,
       settings = {

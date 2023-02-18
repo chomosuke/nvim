37455452b746c45ed83b349648fc3c5d05ad8d70
chomosuke <a13323600@gmail.com>
2023-02-19 03:03:10 +1100

    jdtls debug

--- lua/plugins/lsp-dap/both.lua
+++ lua/plugins/lsp-dap/both.lua
@@ -13,26 +13,50 @@ return function(on_attach, capabilities)
       },
     },
   }
+
   require('flutter-tools').setup {
     lsp = {
       on_attach = on_attach,
       capabilities = capabilities,
     },
   }
+
   require('util').create_autocmds('load_jdtls_upon_ft_java', {
     {
       event = 'FileType',
       opts = {
         pattern = 'java',
         callback = function()
-          require('jdtls').start_or_attach {
+          local mason_registry = require 'mason-registry'
+          local jdtls = require 'jdtls'
+          jdtls.start_or_attach {
             cmd = {
-              require('mason-registry').get_package('jdtls'):get_install_path()
+              mason_registry.get_package('jdtls'):get_install_path()
                 .. '/bin/jdtls',
             },
             root_dir = vim.fs.dirname(
-              vim.fs.find({ '.gradlew', '.git', 'mvnw' }, { upward = true })[1]
+              vim.fs.find(
+                { '.gradlew', '.git', 'mvnw', 'pom.xml' },
+                { upward = true }
+              )[1]
             ),
+            init_options = {
+              bundles = {
+                vim.fn.glob(
+                  mason_registry
+                    .get_package('java-debug-adapter')
+                    :get_install_path()
+                    .. '/extension/server/com.microsoft.java.debug.plugin-*.jar',
+                  true
+                ),
+              },
+            },
+            on_attach = function(client, bufnr)
+              jdtls.setup_dap()
+              require('jdtls.setup').add_commands()
+              on_attach(client, bufnr)
+            end,
+            capabilities = capabilities,
           }
         end,
       },

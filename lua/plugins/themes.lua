1138f8cbfa5afdec1777c41fb6e3f86197ac4e85
chomosuke <a13323600@gmail.com>
2023-04-16 00:20:35 +1000

    reformatted themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,44 +1,32 @@
-return {
+local themes = {
   {
     'Mofiqul/vscode.nvim',
     config = function()
       vim.g.vscode_italic_comment = 1
       vim.g.vscode_disable_nvimtree_bg = true
     end,
-    lazy = false,
-    priority = 1000,
   },
-  {
-    'rmehri01/onenord.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'navarasu/onedark.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'marko-cerovac/material.nvim',
-    lazy = false,
-    priority = 1000,
-  },
+  { 'rmehri01/onenord.nvim' },
+  { 'marko-cerovac/material.nvim' },
   {
     'sainnhe/gruvbox-material',
     config = function()
       vim.gruvbox_material_enable_italic = 1
     end,
-    lazy = false,
-    priority = 1000,
   },
+  { 'projekt0n/github-nvim-theme' },
+  { 'folke/tokyonight.nvim' },
   {
-    'projekt0n/github-nvim-theme',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
+    'navarasu/onedark.nvim',
   },
 }
+
+local plugins = {}
+
+for k, theme in pairs(themes) do
+  theme.lazy = false
+  theme.priority = 1000
+  plugins[k] = theme
+end
+
+return plugins

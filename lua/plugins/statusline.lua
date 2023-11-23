a7f0ae266f648bcfd3ea92993d3d16e2922ed3ec
chomosuke <a13323600@gmail.com>
2023-11-23 19:30:22 +1100

    status line

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,17 +1,79 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
-    lazy = false,
+    event = 'VeryLazy',
     dependencies = {
       'nvim-tree/nvim-web-devicons',
       'arkav/lualine-lsp-progress',
     },
     config = function()
+      local colors = require 'onedark.colors'
+      local ft_termnum = {
+        'filetype',
+        fmt = function(str)
+          ---@diagnostic disable-next-line: undefined-field
+          if vim.b.toggle_number ~= nil then
+            ---@diagnostic disable-next-line: undefined-field
+            str = 'term[' .. vim.b.toggle_number .. ']'
+          end
+          return str
+        end,
+      }
       local sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'filetype' },
-        lualine_c = { 'lsp_progress', 'diagnostics' },
-        lualine_x = { 'diff', 'branch' },
+        lualine_b = { ft_termnum },
+        lualine_c = {
+          {
+            'lsp_progress',
+            colors = {
+              percentage = colors.cyan,
+              title = colors.cyan,
+              message = colors.cyan,
+              spinner = colors.cyan,
+              lsp_client_name = colors.magenta,
+              use = true,
+            },
+            separators = {
+              component = ' ',
+              progress = ' | ',
+              percentage = { pre = '', post = '%% ' },
+              title = { pre = '', post = ': ' },
+              lsp_client_name = { pre = '[', post = ']' },
+              spinner = { pre = '', post = '' },
+              message = {
+                pre = '(',
+                post = ')',
+                commenced = 'In Progress',
+                completed = 'Completed',
+              },
+            },
+            display_components = {
+              'lsp_client_name',
+              'spinner',
+              { 'title', 'percentage', 'message' },
+            },
+            timer = {
+              progress_enddelay = 500,
+              spinner = 1000,
+              lsp_client_name_enddelay = 1000,
+            },
+            spinner_symbols = {
+              '🌑 ',
+              '🌒 ',
+              '🌓 ',
+              '🌔 ',
+              '🌕 ',
+              '🌖 ',
+              '🌗 ',
+              '🌘 ',
+            },
+          },
+          'diagnostics',
+        },
+        lualine_x = {
+          'diff',
+          'branch',
+        },
         lualine_y = {
           function()
             local ff = vim.bo.fileformat
@@ -40,8 +102,7 @@ return {
         },
         sections = sections,
         inactive_sections = {
-          lualine_c = { 'filetype' },
-          lualine_x = {},
+          lualine_c = { ft_termnum },
         },
       }
     end,

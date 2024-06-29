11675d0d44a442a69336d110b09dea06b38e429d
chomosuke <a13323600@gmail.com>
2024-06-30 06:23:04 +1000

    no cpp-exception-breakpoint

--- lua/plugins/lsp-dap/dap.lua
+++ lua/plugins/lsp-dap/dap.lua
@@ -3,12 +3,12 @@ local dap, dapui = require 'dap', require 'dapui'
 require('mason-nvim-dap').setup {
   automatic_setup = { filetypes = { codelldb = { 'c', 'cpp' } } },
   handlers = {
-    require 'mason-nvim-dap'.default_setup,
+    require('mason-nvim-dap').default_setup,
     codelldb = function(config)
-      config.filetypes = {'c', 'cpp'}
-      require 'mason-nvim-dap'.default_setup(config)
-    end
-  }
+      config.filetypes = { 'c', 'cpp' }
+      require('mason-nvim-dap').default_setup(config)
+    end,
+  },
 }
 
 dapui.setup {
@@ -17,16 +17,16 @@ dapui.setup {
       elements = {
         { id = 'scopes', size = 0.55 },
         { id = 'watches', size = 0.25 },
+        { id = 'stacks', size = 0.25 },
         { id = 'breakpoints', size = 0.1 },
-        { id = 'stacks', size = 0.1 },
       },
       size = 40,
       position = 'left',
     },
     {
       elements = {
-        { id = 'console', size = 0.6 },
-        { id = 'repl', size = 0.4 },
+        { id = 'console', size = 0.7 },
+        { id = 'repl', size = 0.3 },
       },
       size = 15,
       position = 'bottom',
@@ -37,4 +37,10 @@ dapui.setup {
 dap.listeners.after.event_initialized['dapui_config'] = function()
   dapui.open {}
   require('nvim-tree.api').tree.close()
+end
+
+dap.listeners.after.event_initialized['no-cpp-except-brkpt'] = function(session)
+  if session.config.type == 'codelldb' then
+    require('dap.repl').execute 'breakpoint name configure --disable cpp_exception'
+  end
 end
